f68af697f1fde2d4a3a87a76213a8e5d1ca35ebd
LyricsLelo <lyricslelo.com@gmail.com>
2026-02-14 22:49:06 +0530

    fix: corrupted import in LoginPage.tsx

--- tally-web-dashboard/src/pages/LoginPage.tsx
+++ tally-web-dashboard/src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-illimport { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { useTheme } from '../contexts/ThemeContext';
