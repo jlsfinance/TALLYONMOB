44814e1493020ca5a4a5f55f32d99dc2dbdeb371
LyricsLelo <lyricslelo.com@gmail.com>
2026-06-18 15:40:36 +0530

    feat: Android release signing config + keystore

--- tally-mobile-app/android/app/build.gradle.kts
+++ tally-mobile-app/android/app/build.gradle.kts
@@ -31,11 +31,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release-key.keystore")
+            storePassword = "password"
+            keyAlias = "key0"
+            keyPassword = "password"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
