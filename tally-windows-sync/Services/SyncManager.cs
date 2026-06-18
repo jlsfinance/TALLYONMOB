3ded95668bd8bcbdb8a7ccd8d7c4933e624f2926
LyricsLelo <lyricslelo.com@gmail.com>
2026-06-18 13:10:20 +0530

    fix: add System.Net.Http using for StringContent/HttpClient in SyncManager

--- tally-windows-sync/Services/SyncManager.cs
+++ tally-windows-sync/Services/SyncManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
